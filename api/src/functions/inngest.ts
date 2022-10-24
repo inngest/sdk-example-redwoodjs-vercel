9732a8a3dbf0ffb45638f95ad69b449fd1d2306f
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-10-24 11:59:38 +0100

    Explicitly name app to be scoped to RedwoodJS for testing

--- api/src/functions/inngest.ts
+++ api/src/functions/inngest.ts
@@ -2,4 +2,4 @@ import { serve } from 'inngest/redwood'
 
 import helloWorld from 'src/inngest/helloWorld'
 
-export const handler = serve('My App', [helloWorld])
+export const handler = serve('My RedwoodJS App', [helloWorld])
